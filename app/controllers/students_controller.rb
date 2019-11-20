2c08ad812fe0de230db558f809ef2d55fa396df6
jstrombe19 <jaredstromberg@gmail.com>
2019-11-20 11:51:05 -0700

    Fully functional strong params on StudentsController.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,33 @@
 class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all 
+    render json: @students 
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render json: @student 
+  end
+
+  def create
+    @student = Student.create(student_params)
+    render json: @student 
+  end
+
+  def update
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy 
+    render json: :index 
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :age, :student_id, :graduated, :major)
+  end
+
 end
